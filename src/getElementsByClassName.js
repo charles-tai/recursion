cad00924cb093e391915dc14c4c188b9a45f4596
Charles Tai <cwtai86@gmail.com>
2014-02-02 19:53:15 -0800

    removed reverse which was causing test to fail

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,29 +6,24 @@
 // };
 
 // But in stead we're going to implement it from scratch:
-var getElementsByClassName =   function (target) {
+function getElementsByClassName(target) {
 
-  var index = 0; // for elementList
   var elementList = []; // list of elements with class that matches target
+  var index = 0; // for elementList
   var location = document.body;
 
   function loop (target,location) {
-    // If location is undefined, location is set as body
-    console.log(location);
     var docElements = location.children;
-    console.log(docElements);
-   // Iterate through document location's children
+    // Iterate through document location's children
     for (var i = 0; i < docElements.length; i++) {
       // Recursive Case: checks if iteration contains children
       if (docElements[i].children.length > 0) {
-        console.log(docElements[i]);
         loop(target, docElements[i]);
       }
-      // Check if contains class
+      // Check if it possesses class
       if(docElements[i].classList.length !== 0) {
-        console.log(docElements[i].className);
-        var namesList = docElements[i].className.split(' ');
         // Check if class matches specified class
+        var namesList = docElements[i].className.split(' ');
         for (var j = 0; j < namesList.length; j++) {
           if (namesList[j] === target) {
             elementList[index] = docElements[i];
@@ -39,5 +34,6 @@ var getElementsByClassName =   function (target) {
     }
   }
   loop(target,location);
+  console.log(elementList);
   return elementList;
 }
